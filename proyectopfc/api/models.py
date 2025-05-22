561dbef0a2ff5527eb3aa523b0619beb1062819c
Josebeth02 <carranzajosebeth@gmail.com>
2025-05-22 11:17:33 -0600

    Implementación de migrations

--- proyectopfc/api/models.py
+++ proyectopfc/api/models.py
@@ -67,21 +67,21 @@ class MetodosPago (models.Model):
     
 class Boleteria (models.Model):
     Usuario = models.CharField(max_length=100)
-    Pago = models.DecimalField(max_length=100, decimal_places=2)
+    Pago = models.DecimalField(max_digits=10, decimal_places=2)
     
     def __str__(self):
         return self.Usuario
     
 class Localidades (models.Model):
     Nombre = models.CharField(max_length=100)
-    Precio = models.DecimalField(max_length=100, decimal_places=2)
+    Precio = models.DecimalField(max_digits=10, decimal_places=2)
     
     def __str__(self):
         return self.Nombre
     
 class DetalleVenta (models.Model):
     Cantidad = models.CharField(max_length=100)
-    Precio = models.DecimalField(max_length=100, decimal_places=2)
+    Precio = models.DecimalField(max_digits=10, decimal_places=2)
     Localidades = models.ForeignKey(Localidades, on_delete=models.CASCADE, related_name="DetalleVenta")
     
     def __str__(self):
@@ -95,7 +95,7 @@ class LocalidadDetalleVenta (models.Model):
         return self.DetalleVenta
     
 class Venta (models.Model):
-    Precio_Total = models.DecimalField(max_length=100, decimal_places=2)
+    Precio_Total = models.DecimalField(max_digits=10, decimal_places=2)
     Usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="Venta")
     Boleteria = models.ForeignKey(Boleteria, on_delete=models.CASCADE, related_name="Venta")
     MetodosPago = models.ForeignKey(MetodosPago, on_delete=models.CASCADE, related_name="Venta")
